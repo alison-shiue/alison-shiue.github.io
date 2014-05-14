e270e2bb552600681975fbfad2e4e8485023b305
alison-shiue <alison@alison-u-pc.(none)>
2014-05-14 10:07:05 +0800

    test

--- ffos/js/Dimension-Bugzilla.js
+++ ffos/js/Dimension-Bugzilla.js
@@ -18,40 +18,19 @@ Dimension.addEdges(true,  Mozilla, [
 			{"name":"Reopened", "esfilter":{"term":{"bug_status":"reopened"}}},
 			{"name":"Other", "esfilter":{"not":{"terms":{"bug_status":["resolved", "verified", "closed"]}}}}
 		]},
-                {"name":"Invalid", "partitions":[
-                        {"name":"Resolved",
-                              "esfilter":{"term":{"bug_status":"resolved"}},
-                              "field":"resolution", "partitions":[
-                                      {"name":"Invalid", "value":"invalid", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"invalid"}}},
-                                      {"name":"Won't Fix", "value":"wontfix", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"wontfix"}}},
-                                      {"name":"WorksForMe", "value":"worksforme", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"worksforme"}}}
-                              ],
-                              "key":"value",
-                              "value":"name"
-                        },
-                ]},
-                {"name":"CanVerify", "partitions":[
-                        {"name":"Resolved",
-                                "esfilter":{"term":{"bug_status":"resolved"}},
-                                "field":"resolution", "partitions":[
-                                        {"name":"Fixed", "value":"fixed", "style":{}, "esfilter":{"term":{"resolution":"fixed"}}},
-                                        {"name":"Duplicate", "value":"duplicate", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"duplicate"}}}
-                                ],
-                                "key":"value",
-                                "value":"name"
-                        }
-                ]},
 		{"name":"Closed", "partitions":[
-			//{"name":"Resolved",
-			//	"esfilter":{"term":{"bug_status":"resolved"}},
-			//	"field":"resolution", "partitions":[
-			//		{"name":"Invalid", "value":"invalid", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"invalid"}}},
-			//		{"name":"Won't Fix", "value":"wontfix", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"wontfix"}}},
-			//		{"name":"WorksForMe", "value":"worksforme", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"worksforme"}}}
-			//	],
-			//	"key":"value",
-			//	"value":"name"
-			//},
+			{"name":"Resolved",
+				"esfilter":{"term":{"bug_status":"resolved"}},
+				"field":"resolution", "partitions":[
+					{"name":"Fixed", "value":"fixed", "style":{}, "esfilter":{"term":{"resolution":"fixed"}}},
+					{"name":"Duplicate", "value":"duplicate", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"duplicate"}}},
+					{"name":"Invalid", "value":"invalid", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"invalid"}}},
+					{"name":"Won't Fix", "value":"wontfix", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"wontfix"}}},
+					{"name":"WorksForMe", "value":"worksforme", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"worksforme"}}}
+				],
+				"key":"value",
+				"value":"name"
+			},
 			{"name":"Verified", "esfilter":{"term":{"bug_status":"verified"}}},
 			{"name":"Closed", "esfilter":{"term":{"bug_status":"closed"}}}
 		]}
