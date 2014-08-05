369e724611e2fd486f9800349a0449c6b8615b61
alison-shiue <alison@alison-u-pc.(none)>
2014-08-05 18:45:59 +0800

    rollback WORKSFORME bug fixing

--- ffos/js/Dimension-Bugzilla.js
+++ ffos/js/Dimension-Bugzilla.js
@@ -18,9 +18,21 @@ Dimension.addEdges(true,  Mozilla, [
 			{"name":"Reopened", "esfilter":{"term":{"bug_status":"reopened"}}},
 			{"name":"Other", "esfilter":{"not":{"terms":{"bug_status":["resolved", "verified", "closed"]}}}}
 		]},
+		/*
 		{"name":"CanVerify", "partitions":[
                         {"name":"Resolved",
                                 "esfilter":{"term":{"bug_status":"resolved", "resolution":"fixed", "resolution":"duplicate"}},
+                                "field":"resolution", "partitions":[
+                                        {"name":"Fixed", "value":"fixed", "style":{}, "esfilter":{"term":{"resolution":"fixed"}}},
+                                        {"name":"Duplicate", "value":"duplicate", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"duplicate"}}}
+                                ],
+                                "key":"value",
+                                "value":"name"
+                        }
+                ]},*/
+		{"name":"CanVerify", "partitions":[
+                        {"name":"Resolved",
+                                "esfilter":{"term":{"bug_status":"resolved"}},
                                 "field":"resolution", "partitions":[
                                         {"name":"Fixed", "value":"fixed", "style":{}, "esfilter":{"term":{"resolution":"fixed"}}},
                                         {"name":"Duplicate", "value":"duplicate", "style":{"visibility":"hidden"}, "esfilter":{"term":{"resolution":"duplicate"}}}
